1a9d6c01975195881404ad5a0030c97833a74f9b
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2015-10-30 15:24:29 -0700

    Replaced food groups with descriptions so that the description could be devise instead of specific for the user.

--- Food Tracker Application/Input Table.js
+++ Food Tracker Application/Input Table.js
@@ -4,12 +4,9 @@ $(document).ready(function(){
         var aDate = $('#aDate')[0].value;
         var eDate = $('#eDate')[0].value;
         var weight = $('#Weight')[0].value;
-        var fruit = $('#fruit')[0].value;
-        var vegetable = $('#vegetable')[0].value;
-        var meat = $('#meat')[0].value;
-        var fish = $("#fish")[0].value;
+        var description = $('#description')[0].value;
         var submit = $("#submit");
         $("#trackerTable").append("<tr><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
-            "<td>" + fruit + vegetable + meat + fish + "</td></tr>");
+            "<td>" + description + "</td></tr>");
     });
 });
