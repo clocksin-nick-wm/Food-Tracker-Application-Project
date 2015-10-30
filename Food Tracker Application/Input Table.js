1cc77da6a596f4c0de69b09ffbf71410eee7e3d8
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2015-10-30 15:21:35 -0700

    Created an integrated CSS page to align the content of the food tracker part and also added a background to represent the fruit.

--- Food Tracker Application/Input Table.js
+++ Food Tracker Application/Input Table.js
@@ -1,18 +1,15 @@
-var foodItem = $("#foodItem").value;
-var aDate = $("#aDate").value;
-var eDate = $("#eDate").value;
-var color = $("#Color").value;
-var weight = $("#Weight").value;
-var fruit = $("#fruit").value;
-var vegetable = $("#vegetable").value;
-var meat = $("#meat").value;
-var fish = $("#fish").value;
-var submit = $("#submit");
-
-
 $(document).ready(function(){
     $(submit).click(function(){
-        $("#trackerTable").append("<tr><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + color + "</td><td>" + weight + "</td>" +
+        var foodItem = document.getElementById('foodItem').value;
+        var aDate = $('#aDate')[0].value;
+        var eDate = $('#eDate')[0].value;
+        var weight = $('#Weight')[0].value;
+        var fruit = $('#fruit')[0].value;
+        var vegetable = $('#vegetable')[0].value;
+        var meat = $('#meat')[0].value;
+        var fish = $("#fish")[0].value;
+        var submit = $("#submit");
+        $("#trackerTable").append("<tr><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
             "<td>" + fruit + vegetable + meat + fish + "</td></tr>");
     });
 });
