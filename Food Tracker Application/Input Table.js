362d18fa5e0ad72c5dedbd7407130e98af8783ea
Nicolas Clocksin <nicolas.clocksin@west-mec.org>
2015-11-01 14:21:28 -0700

    Added a new section called calories to help track the calories consumed by a person per day. The section is unfinished but will be in a later version.

--- Food Tracker Application/Input Table.js
+++ Food Tracker Application/Input Table.js
@@ -7,14 +7,27 @@ $(document).ready(function(){
         var description = $('#description')[0].value;
         var day = $('#day')[0].value;
         var submit = $("#submit");
+        /* when the submit button for the first variable is clicked the variables that are contained in the ids will be transferred
+         to a table row that will be created in trackerTable*/
         $("#trackerTable").append("<tr><td>" + day + "</td><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
             "<td>" + description + "</td></tr>");
     });
+    // when submit is clicked the table rows will move down
     $(submit).click(function(){
       $("#health").animate({
           'marginTop' : "+=23px"  //moves down
         })
     });
+    $(submit).click(function(){
+        $("#calories").animate({
+            'marginTop' : "+=23px"  //moves down
+        })
+    });
+    // this portion below is a work in progress
+    var submitCalories = $("#submit2");
+    $(submitCalories).click(function(){
+        var dayCalories = $('#days')[0].value;
+        var calorie = $('#calorie')[0].value;
+        $("#calorieTracker").append("<tr><td>" + dayCalories + " </td><td>" + calorie + "</td></tr>");
+    });
 });
-
-$
