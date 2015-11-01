d363421f0af8859b8aa7fb3e3e7c4db4ee746006
Nicolas Clocksin <nicolas.clocksin@west-mec.org>
2015-10-31 17:12:36 -0700

    Added animation to make the health tips move down when adding a table row. Adjusted the position of the table and health. Also added new option for dieting.

--- Food Tracker Application/Input Table.js
+++ Food Tracker Application/Input Table.js
@@ -5,8 +5,16 @@ $(document).ready(function(){
         var eDate = $('#eDate')[0].value;
         var weight = $('#Weight')[0].value;
         var description = $('#description')[0].value;
+        var day = $('#day')[0].value;
         var submit = $("#submit");
-        $("#trackerTable").append("<tr><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
+        $("#trackerTable").append("<tr><td>" + day + "</td><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
             "<td>" + description + "</td></tr>");
     });
+    $(submit).click(function(){
+      $("#health").animate({
+          'marginTop' : "+=23px"  //moves down
+        })
+    });
 });
+
+$
