59fa606fae57440b00194bffac23ce7a0e57b088
Nicolas Clocksin <nicolas.clocksin@west-mec.org>
2015-11-01 20:30:55 -0700

    Still have a bug that creates and removes the tables row to calories.

--- Food Tracker Application/Calories Table.js
+++ Food Tracker Application/Calories Table.js
@@ -1,8 +1,8 @@
-$(document).ready( function(){
+// There is a problem with the function where it adds a table row in the calories/second table but automatically removes it //
+
     var submitCalories = $("#submit2");
     $(submitCalories).click(function(){
         var dayCalories = document.getElementById('days').value;
         var calorie = document.getElementById('calorie').value;
-        $("#calorieTracker").append("<tr><td>" + dayCalories + " </td><td>" + calorie + "</td></tr>");
+        $("#calorieTracker").append("<tr><td>" + dayCalories + "</td><td>" + calorie + "</td></tr>");
     });
-});
--- Food Tracker Application/Input Table.js
+++ Food Tracker Application/Input Table.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $(submit).click(function(){
+$(document).ready(function() {
+    var submit = $("#submit");
+    $(submit).click(function () {
         var foodItem = document.getElementById('foodItem').value;
         var aDate = $('#aDate')[0].value;
         var eDate = $('#eDate')[0].value;
@@ -7,26 +8,26 @@ $(document).ready(function(){
         var servingSize = $('#description')[0].value;
         var day = $('#day')[0].value;
         var consumed = $('#consumed')[0].value;
-        var submit = $("#submit");
         /* when the submit button for the first variable is clicked the variables that are contained in the ids will be transferred
          to a table row that will be created in trackerTable*/
         $("#trackerTable").append("<tr><td>" + day + "</td><td>" + foodItem + "</td><td>" + aDate + "</td><td>" + eDate + "</td><td>" + weight + "</td>" +
             "<td>" + servingSize + "</td><td>" + consumed + "</td></tr>");
+
     });
     // when submit is clicked the table rows will move down
-    $(submit).click(function(){
-      $("#health").animate({
-          'marginTop' : "+=23px"  //moves down
+    $(submit).click(function () {
+        $("#health").animate({
+            'marginTop': "+=23px"  //moves down
         })
     });
-    $(submit).click(function(){
+    $(submit).click(function () {
         $("#calories").animate({
-            'marginTop' : "+=23px"  //moves down
+            'marginTop': "+=23px"  //moves down
         })
     });
-    $(submit).click(function(){
+    $(submit).click(function () {
         $("#caloriesTable").animate({
-            'marginTop' : "+=23px"  //moves down
+            'marginTop': "+=23px"  //moves down
         });
     });
 });
